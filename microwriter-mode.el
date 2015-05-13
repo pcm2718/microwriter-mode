706983dc8baa40f4f16ebcba3689300699a1447f
Parker Michaelson <parker.michaelson@gmail.com>
2015-05-13 11:55:09 -0600

    Misc improvements.

--- microwriter-mode.el
+++ microwriter-mode.el
@@ -1,12 +1,31 @@
 ;;;; TODO Might be useful overall to steal more ideas from
 ;;;; key-chord.el .
 
-(defvar microwriter-mode-delay 0.5
+;;;; TODO Use 1-6 (and others) as bucky bits, queued of course. cmd in
+;;;; combination with a character should produce C-<char> . cmd in
+;;;; combination with bucky bits should either set a bucky bit for the
+;;;; next char, cmd itself, or maybe a mode switcher? Lots of chording
+;;;; possibilities for the bucky bits. The keys above normal positions
+;;;; work with the same hand arrangement. The most important thing is
+;;;; to provide sane defaults for common chords and bucky bits, while
+;;;; providing a systematic method for less common chords and a good
+;;;; ELisp interface for expansion and customization.
+
+;;;; TODO Would it be smart to allow multi-chord operations, for caps
+;;;; and such (iterate along the modes)? Or just to encourage all
+;;;; lowercase?
+
+;;;; testing the microwriter, once more unto the breach
+
+;;;; TODO Need to build a typing trainer for the chording keyboard.
+
+(defvar microwriter-mode-delay 0.1
   "The maximum delay between microwriter-mode key releases.")
 
 (make-variable-buffer-local
    (defvar microwriter-mode-pressed-keys '()
      "The keys pressed since the last character entry event."))
+
 
 ;; (defun print-elements-of-list (list)
 ;;   "Print each element of LIST on a line of its own."
@@ -25,76 +44,82 @@
   (append
    (cons 'cond
          ;; TODO Reverse the fingers-character order?
-         (mapcar (lambda (x) `((sets-equal ,condvar ',(first x)) ,(second x)))
+         (mapcar (lambda (x) `((sets-equal ,condvar ',(first x)) '(insert ,(second x))))
                  body))
     ;; ,(mapcar (lambda (x) (list (list 'sets-equal condvar (first x)) (second x))) body)
     ;; TODO Fix this to be more general.
     '((t ""))))
 
-;; TODO Trigger the timer event only on key release?
+;; TODO Currently the command parser is a possible recursive
+;; disaster. Write a mini-language for describing chords.
+(defun microwriter-mode-get-action (keys)
+  (cond
+   ((sets-equal keys '(cmd)) "cmd")
+   ((member 'cmd keys)
+    (let ((action (microwriter-mode-get-action (remq 'cmd keys))))
+      `(insert ,(capitalize (cadr action)))))
+   (t (case-sets-equal keys
+                       ((index middle) "a")
+                       ((middle ring pinky) "b")
+                       ((thumb middle) "c")
+                       ((thumb index middle) "d")
+                       ((index) "e")
+                       ((thumb index middle ring) "f")
+                       ((ring pinky) "g")
+                       ((thumb pinky) "h")
+                       ((thumb index) "i")
+                       ((thumb ring pinky) "j")
+                       ((thumb ring) "k")
+                       ((thumb index pinky) "l")
+                       ((index middle ring pinky) "m")
+                       ((middle ring) "n")
+                       ((middle) "o")
+                       ((thumb index middle ring pinky) "p")
+                       ((middle pinky) "q")
+                       ((thumb index ring) "r")
+                       ((ring) "s")
+                       ((index ring) "t")
+                       ((pinky) "u")
+                       ((index pinky) "v")
+                       ((thumb index ring pinky) "w")
+                       ((thumb middle ring pinky) "x")
+                       ((thumb middle ring) "y")
+                       ((thumb middle pinky) "z")
+                       ((thumb) " ")
+                       ((index middle ring) ".")
+                       ((index middle pinky) "-")
+                       ((index ring pinky) ",")
+                       ((thumb index middle pinky) "'")))))
+
+;; Todo Trigger the timer event only on key release?
 (defun microwriter-mode-key-handler (keysym)
   (interactive)
   (add-to-list 'microwriter-mode-pressed-keys keysym)
   (when (sit-for microwriter-mode-delay 'no-redisplay)
-      ;; (print-elements-of-list microwriter-mode-pressed-keys)
     ;; TODO Rewrite this to use a macro later.
-    (insert
-     (case-sets-equal microwriter-mode-pressed-keys
-                      ((index middle) "a")
-                      ((middle ring pinky) "b")
-                      ((thumb middle) "c")
-                      ((thumb index middle) "d")
-                      ((index) "e")
-                      ((thumb index middle ring) "f")
-                      ((ring pinky) "g")
-                      ((thumb pinky) "h")
-                      ((thumb index) "i")
-                      ((thumb ring pinky) "j")
-                      ((thumb ring) "k")
-                      ((thumb index pinky) "l")
-                      ((index middle ring pinky) "m")
-                      ((middle ring) "n")
-                      ((middle) "o")
-                      ((thumb index middle ring pinky) "p")
-                      ((middle pinky) "q")
-                      ((thumb index ring) "r")
-                      ((ring) "s")
-                      ((index ring) "t")
-                      ((pinky) "u")
-                      ((index pinky) "v")
-                      ((thumb index ring pinky) "w")
-                      ((thumb middle ring pinky) "x")
-                      ((thumb ring pinky) "y")
-                      ((thumb middle pinky) "z")
-                      ((thumb) " ")
-                      ((index middle ring) ".")
-                      ((index middle pinky) "-")
-                      ((index ring pinky) ",")
-                      ((thumb index middle pinky "'"))))
-     ;; (cond
-     ;;  ((sets-equal microwriter-mode-pressed-keys '(index middle)) "a")
-     ;;  (t "")))
+    (eval (microwriter-mode-get-action microwriter-mode-pressed-keys))
     (setq microwriter-mode-pressed-keys '())))
+(defvar microwriter-mode-map
+  (let ((map (make-sparse-keymap)))
+    ;; TODO Consider swaping cmd and thumb .
+    ;; First test message:hello world.
+    (define-key map (kbd "<kp-insert>")
+      (lambda () (interactive) (microwriter-mode-key-handler 'cmd)))
+    (define-key map (kbd "<kp-end>")
+      (lambda () (interactive) (microwriter-mode-key-handler 'thumb)))
+    (define-key map (kbd "<kp-home>")
+      (lambda () (interactive) (microwriter-mode-key-handler 'index)))
+    (define-key map (kbd "<kp-up>")
+      (lambda () (interactive) (microwriter-mode-key-handler 'middle)))
+    (define-key map (kbd "<kp-prior>")
+      (lambda () (interactive) (microwriter-mode-key-handler 'ring)))
+    (define-key map (kbd "<kp-add>")
+      (lambda () (interactive) (microwriter-mode-key-handler 'pinky)))
+    map))
 
 ;;;###autoload
 (define-minor-mode microwriter-mode
   "A mode emulating the Microwriter chording keyboard."
-  :lighter " Microwriter"
-  :keymap (let ((map (make-sparse-keymap)))
-            ;; TODO Consider swaping cmd and thumb .
-            ;; First test message:hello world.
-            (define-key map (kbd "<kp-insert>")
-              (lambda () (interactive) (microwriter-mode-key-handler 'cmd)))
-            (define-key map (kbd "<kp-end>")
-              (lambda () (interactive) (microwriter-mode-key-handler 'thumb)))
-            (define-key map (kbd "<kp-home>")
-              (lambda () (interactive) (microwriter-mode-key-handler 'index)))
-            (define-key map (kbd "<kp-up>")
-              (lambda () (interactive) (microwriter-mode-key-handler 'middle)))
-            (define-key map (kbd "<kp-prior>")
-              (lambda () (interactive) (microwriter-mode-key-handler 'ring)))
-            (define-key map (kbd "<kp-add>")
-              (lambda () (interactive) (microwriter-mode-key-handler 'pinky)))
-            map))
+  :lighter " Microwriter")
 
 (provide 'microwriter-mode)
